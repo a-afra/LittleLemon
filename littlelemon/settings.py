a68f08b40ffa2e5539f092e55bc43ed3aea846f6
a-afra <afraaalireza@gmail.com>
2023-08-21 18:12:36 +0330

    Upate settings.py for easier review

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': config('DB_PASSWORD'),
+        'PASSWORD': 'DB_PASSWORD',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
